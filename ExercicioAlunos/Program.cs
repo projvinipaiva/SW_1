19d834f590b0120a12a3a7e6e08f9ae54d3a960c
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-03-14 12:06:44 -0300

    Add files via upload

--- ExercicioAlunos/Program.cs
+++ ExercicioAlunos/Program.cs
@@ -4,20 +4,21 @@ class Program
     static void Main(string[] args)
     {
         Alunos aluno1= new Alunos();
-       //nome
+       //NOME DO ALUNO
+
         Console.WriteLine("Digite seu nome:");
         aluno1.nome = Console.ReadLine();
 
-       //nota 1
+       //NOTA 1 DA MÉDIA
         Console.WriteLine("Digite sua primeira nota:");
         aluno1.nota1 = double.Parse(Console.ReadLine());
        
-       //nota 2
+       //NOTA 2 DA MÉDIA
         Console.WriteLine("Digite sua segunda nota:");
         aluno1.nota2 = double.Parse(Console.ReadLine());
 
 
-        //mensagem
+        //MENSAGEM("Retorno")
         aluno1.mensagem();
     }
 }
