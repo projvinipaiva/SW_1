0f1ae9568acd7fe0b4f79669e1eedd0db559462c
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-03-21 12:27:51 -0300

    Add files via upload

---
+++ banco/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace banco;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Banco Etec MCM!");
+        Conta c = new Conta();
+       // c.Saldo = 1500;
+       c.Depositar(1500);
+
+       c.AjustaLimite(500);
+
+        //Console.WriteLine("seu saldo é de: " + c.Saldo + " e seu limite é de: "  + c.Limite);
+        Console.WriteLine("seu saldo é de: " + c.MostrarSaldo());
+    }
+}
