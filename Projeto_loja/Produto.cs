c99e0d1a8519329eb019537f1748f566668c6591
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-03-28 12:27:34 -0300

    Add files via upload

---
+++ Projeto_loja/Produto.cs
@@ -0,0 +1,37 @@
+﻿namespace Projeto_loja; 
+class Produto
+{
+
+    public Produto(){
+        this.Nome = "Rodolfo";
+        this.Preco = 15;    
+    }
+
+     public Produto(string nome){
+        this.Nome = nome;
+        this.Preco = 10;    
+    }
+     
+     public Produto(string nome, int desconto){
+        this.Nome = nome;
+        this.Preco = 10 - (10*desconto/100);    
+    }
+    
+        private string? Nome {get;set;}
+
+        private double Preco {get;set;}
+
+        public void AlteraNome( string nome){
+            this.Nome = nome;
+        }
+        
+        public void alteraPreco(double preco){
+            this.Preco = preco;
+        }
+
+        public void MostraDados(){
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Preço:" + this.Preco);
+        }
+    
+}
---
+++ Projeto_loja/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto_loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Produto p1 = new Produto();
+       // Produto p1 = new Produto("XYZ");
+        Produto p1 = new Produto("ABC", 10);
+        p1.MostraDados();
+    }
+}
