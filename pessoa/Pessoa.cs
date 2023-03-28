ba5ba098d399fff006c546cd8f81031c4939b7a6
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-03-28 12:01:52 -0300

    Add files via upload

---
+++ pessoa/Pessoa.cs
@@ -0,0 +1,39 @@
+﻿namespace pessoa;
+class Pessoa
+{
+    public Pessoa(){
+        this.Nome = "NÃO INFORMADO";
+        this.SobreNome = "NÃO INFORMADO";
+        this.Idade = 0;
+    }
+
+       public Pessoa(string nome, int idade){
+        this.Nome = nome;
+        this.SobreNome = "NÃO INFORMADO";
+        this.Idade = idade;
+    }
+
+    private string? Nome {get;set;}
+    private string? SobreNome {get;set;}
+    private int Idade {get; set;}
+
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+
+    public void AlteraSobreNome(string SobreNome){
+        this.SobreNome = SobreNome;
+    }
+
+    public void AlteraIdade(int idade){
+        this.Idade = idade;
+    }
+
+    public void MostraDados(){
+        Console.WriteLine("nome: " + this.Nome);
+        Console.WriteLine("SobreNome: " + this.SobreNome);
+        Console.WriteLine("Idade: " + this.Idade);
+    }
+
+
+}
---
+++ pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("GOONDOAFREDO", 18);
+        //p1.AlteraNome("ANDERSON");
+        //p1.AlteraSobreNome("VANIN");
+        //p1.AlteraIdade(48);
+        p1.MostraDados();
+    }
+}
