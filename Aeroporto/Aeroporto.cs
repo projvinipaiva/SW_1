377ac7497418370df2b4d9a4c182dd75aafc0975
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-04-04 10:28:20 -0300

    Add files via upload

---
+++ Aeroporto/Aeroporto.cs
@@ -0,0 +1,56 @@
+﻿namespace Aeroporto;
+class Aeroporto
+{   
+
+     public Aeroporto(int codigo, string nome)
+    {
+        this.Codigo = codigo;
+        this.Nome = nome;
+        this.Entrada = "Não informado";
+        this.Saida = "Não informado";
+        
+    }
+
+    public Aeroporto(int codigo, string nome, string entrada, string saida)
+    {
+        this.Codigo = codigo;
+        this.Nome = nome;
+        this.Entrada = entrada;
+        this.Saida = saida;
+        
+    }
+
+    public Aeroporto()
+    {
+    }
+
+    private int Codigo {get; set;}
+     private string? Nome {get; set;}
+     private string? Entrada {get; set;}
+     private string? Saida {get; set;}
+
+      public void AlteraCodigo(int codigo){
+        this.Codigo = codigo;
+    }
+
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+
+    public void AlteraEntrada(string entrada){
+        this.Entrada = entrada;
+    }
+
+    public void AlteraSaida(string saida){
+        this.Saida = saida;
+    }
+
+  public void MostraDados(){
+        Console.WriteLine("Código: " + this.Codigo);
+        Console.WriteLine("Nome: " + this.Nome);
+        Console.WriteLine("Entrada: " + this.Entrada);
+        Console.WriteLine("Saída: " + this.Saida);
+
+    }
+
+}
---
+++ Aeroporto/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aeroporto;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aeroporto a1 = new Aeroporto();
+        a1.AlteraCodigo(1);
+        a1.AlteraNome("Quatar Airlines");
+        // a1.AlteraEntrada("Tupi");
+        // a1.AlteraSaida("Jua");
+        a1.MostraDados();
+
+    }
+}
